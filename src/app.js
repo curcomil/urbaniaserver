6e47fffbb88bb9a4d0323c342fc429a3016610bb
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-10-01 11:26:12 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express, { json } from "express";
-
+import cors from "cors";
 const app = express;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "", // Añade aquí tu localhost
+  })
+);
 
 app.use("/api");
 
