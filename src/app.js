47d3464d7fe6236636d464a92f4cfe72408f6850
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-10-14 15:49:55 -0600

    fix type

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express, { json } from "express";
 import cors from "cors";
-const app = express;
+
+const app = express(); // Aquí invocas la función
 
 app.use(
   cors({
     credentials: true,
-    origin: "", // Añade aquí tu localhost
+    origin: "", // Añade aquí tu localhost o el dominio permitido
   })
 );
 
-app.use("/api");
+app.use("/api", json()); // Asegúrate de usar json middleware para manejar JSON en las peticiones
 
 export default app;
