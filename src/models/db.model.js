bdb954b95868bac48536aa183e2233c728e3388b
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-11-05 15:12:08 -0600

    Create db.model.js

---
+++ src/models/db.model.js
@@ -0,0 +1,67 @@
+const dbSchema = {
+  Nombre: { type: String, required: true },
+  Sotanos: { type: Number, required: true },
+  Niveles: { type: Number, required: true },
+  Edificios: { type: Number, required: true },
+  Fechas: {
+    Plan: {
+      Inicio: { type: Date, required: true },
+      Fin: { type: Date, required: true },
+    },
+    Ejecucion: {
+      Inicio: { type: Date },
+      Fin: { type: Date },
+    },
+  },
+  Etapas: [
+    {
+      Partidas: [
+        {
+          Nombre: { type: String },
+          Subpartidas: { type: Array, default: [] },
+          Fechas: {
+            Plan: {
+              Inicio: { type: Date },
+              Fin: { type: Date },
+            },
+            Ejecucion: {
+              Inicio: { type: Date },
+              Fin: { type: Date },
+            },
+          },
+        },
+      ],
+      Edificios: [
+        {
+          Nombre: { type: String },
+          Partidas: [
+            {
+              Nombre: { type: String },
+              Subpartidas: { type: Array, default: [] },
+              Fechas: {
+                Plan: {
+                  Inicio: { type: Date },
+                  Fin: { type: Date },
+                },
+                Ejecucion: {
+                  Inicio: { type: Date },
+                  Fin: { type: Date },
+                },
+              },
+            },
+          ],
+          Fechas: {
+            Plan: {
+              Inicio: { type: Date },
+              Fin: { type: Date },
+            },
+            Ejecucion: {
+              Inicio: { type: Date },
+              Fin: { type: Date },
+            },
+          },
+        },
+      ],
+    },
+  ],
+};
