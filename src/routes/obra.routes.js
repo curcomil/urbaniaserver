b96955c79a5ea4ae1d4c13bd1741da53ec377051
curcomil <sadielsalgado98@gmail.com>
2024-11-29 21:32:15 -0600

    Enobra connect

--- src/routes/obra.routes.js
+++ src/routes/obra.routes.js
@@ -3,6 +3,10 @@ import {
   getAllObras,
   getObraById,
   new_proyect,
+  deletePartida,
+  deleteSubpartida,
+  updateSubpartidaFecha,
+  updatePartidaFecha,
 } from "../controllers/obra.controller.js";
 
 const router = express.Router();
@@ -15,5 +19,17 @@ router.get("/obras/:id", getObraById);
 
 // Ruta para crear una nueva obra
 router.post("/nueva", new_proyect);
+
+//Ruta para eliminar una partida
+router.delete("/deletePartida", deletePartida);
+
+//Ruta para eliminar una subpartida
+router.delete("/deleteSubpartida", deleteSubpartida);
+
+//Ruta para fechas de una subpartida
+router.post("/fechaSubpartida", updateSubpartidaFecha);
+
+//Ruta para fechas de una partida
+router.post("/fechaPartida", updatePartidaFecha);
 
 export default router;
